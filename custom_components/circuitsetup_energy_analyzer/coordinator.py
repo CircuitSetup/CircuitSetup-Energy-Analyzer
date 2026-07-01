1d798fbde22d33f5a2b44e2f3a7ea8a8122045a0
jdeglavina <jdeg@sugarmanstudios.com>
2026-07-01 02:27:34 -0400

    Move advanced settings readback into settings controller

--- custom_components/circuitsetup_energy_analyzer/coordinator.py
+++ custom_components/circuitsetup_energy_analyzer/coordinator.py
@@ -1556,74 +1556,7 @@ class EnergyAnalyzerCoordinator(DataUpdateCoordinator):
         )
 
     def _advanced_settings_for_circuit(self: Self, circuit_id: str) -> dict[str, Any]:
-        settings: dict[str, Any] = {}
-        for source in (
-            self.entry_data.get(CONF_ADVANCED_SETTINGS, {}),
-            self.options.get(CONF_ADVANCED_SETTINGS, {}),
-        ):
-            if not isinstance(source, Mapping):
-                continue
-            raw_settings = source.get(circuit_id, {})
-            if isinstance(raw_settings, Mapping):
-                settings.update(dict(raw_settings))
-
-        settings.update(
-            self.store_data.energy_usage_settings_by_circuit.get(circuit_id, {}),
-        )
-        settings.update(
-            self.store_data.activity_alert_settings_by_circuit.get(circuit_id, {}),
-        )
-        settings.update(self.store_data.demand_settings_by_circuit.get(circuit_id, {}))
-        settings.update(
-            self.store_data.capacity_settings_by_circuit.get(circuit_id, {}),
-        )
-        settings.update(self.store_data.standby_settings_by_circuit.get(circuit_id, {}))
-        settings.update(
-            self.store_data.metric_consistency_settings_by_circuit.get(
-                circuit_id,
-                {},
-            ),
-        )
-
-        leg_imbalance = self.store_data.leg_imbalance_settings_by_circuit.get(
-            circuit_id,
-            {},
-        )
-        if "warning_ratio" in leg_imbalance:
-            settings["leg_imbalance_warning_ratio"] = leg_imbalance["warning_ratio"]
-        if "minimum_total_power_w" in leg_imbalance:
-            settings["leg_imbalance_min_total_power_w"] = leg_imbalance[
-                "minimum_total_power_w"
-            ]
-
-        balance = self.store_data.balance_settings_by_circuit.get(circuit_id, {})
-        if "negative_tolerance_w" in balance:
-            settings["balance_negative_tolerance_w"] = balance[
-                "negative_tolerance_w"
-            ]
-
-        solar_flow = self.store_data.solar_flow_settings_by_circuit.get(
-            circuit_id,
-            {},
-        )
-        if "export_tolerance_w" in solar_flow:
-            settings["solar_export_tolerance_w"] = solar_flow["export_tolerance_w"]
-        for key in (
-            "solar_surplus_threshold_w",
-            "high_solar_surplus_threshold_w",
-            "flexible_load_running_threshold_w",
-        ):
-            if key in solar_flow:
-                settings[key] = solar_flow[key]
-
-        settings.update(
-            self.store_data.operating_detection_settings_by_circuit.get(
-                circuit_id,
-                {},
-            )
-        )
-
-        return settings
+        return self.settings_controller.advanced_settings_for_circuit(circuit_id)
 
     def _advisor_feature_history_for_circuit(
         self: Self,
